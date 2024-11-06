8a77058a2ab593a309b2f0aa0e6cc1bd93aa8ccc
Gabriel Alcantara Alves dos Santos <141934868+GabrielAlcantara01@users.noreply.github.com>
2024-11-06 00:39:12 +0000

    inicio do trabalho

---
+++ index.php
@@ -0,0 +1,22 @@
+<head>
+        <link rel="stylesheet" href="style.css" type="text/css"/>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <h1>Jogo da velha</h1>
+        <div class="jogo">
+            <div class="casa"></div>
+            <div class="casa"></div>
+            <div class="casa"></div>
+            <div class="casa"></div>
+            <div class="casa"></div>
+            <div class="casa"></div>
+            <div class="casa"></div>
+            <div class="casa"></div>
+            <div class="casa"></div>
+        </div>
+    </body>
+</html>
